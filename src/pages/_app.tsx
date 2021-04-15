d50cdd7bf0548d4549ed167f2b09a13e29c247a7
Felipe Jung <felipemattoseu@gmail.com>
2021-04-15 10:59:20 -0400

    Add miragejs fake api

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
+import "swiper/swiper-bundle.min.css";
+import "styles/global.css";
+
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import { theme } from "styles/theme";
 
-import "swiper/swiper-bundle.min.css";
-import "styles/global.css";
+import { makeServer } from "services/mirage";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer({ environment: "development" });
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
---
+++ src/services/mirage.ts
@@ -0,0 +1,72 @@
+import { createServer, Model } from "miragejs";
+
+const continents = [
+  {
+    slug: "north-america",
+    title: "América do Norte",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    slug: "south-america",
+    title: "América do Sul",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    slug: "asia",
+    title: "América do Sul",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    slug: "africa",
+    title: "Áfria",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    slug: "europe",
+    title: "Europa",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    slug: "oceania",
+    title: "América do Sul",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+];
+
+export function makeServer({ environment = "test" }) {
+  const server = createServer({
+    environment,
+
+    models: {
+      continent: Model,
+    },
+
+    seeds(server) {
+      server.db.loadData({
+        continents,
+      });
+    },
+
+    routes() {
+      this.namespace = "api";
+
+      this.get("/continents", () => {
+        return this.schema.all("continent");
+      });
+    },
+  });
+
+  return server;
+}

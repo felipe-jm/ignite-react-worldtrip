dfca2c803b34c6d9afad1a4bd4fd6ba5872e9d62
Felipe Jung <felipemattoseu@gmail.com>
2021-04-16 09:58:18 -0400

    Update miragejs data

--- src/services/mirage.ts
+++ src/services/mirage.ts
@@ -1,12 +1,19 @@
 import { createServer, Model } from "miragejs";
 
-const continents = [
+import { ContinentType } from "templates/Continent";
+
+export const continents: ContinentType[] = [
   {
     slug: "north-america",
     title: "América do Norte",
     subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    countries: 23,
+    languages: 23,
+    cities: 27,
+    imgUrl:
+      "https://images.unsplash.com/photo-1496442226666-8d4d0e62e6e9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
   },
   {
     slug: "south-america",
@@ -14,20 +21,35 @@ const continents = [
     subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    countries: 14,
+    languages: 23,
+    cities: 27,
+    imgUrl:
+      "https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
   },
   {
     slug: "asia",
-    title: "América do Sul",
+    title: "Ásia",
     subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    countries: 48,
+    languages: 23,
+    cities: 27,
+    imgUrl:
+      "https://images.unsplash.com/photo-1480796927426-f609979314bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80",
   },
   {
     slug: "africa",
-    title: "Áfria",
+    title: "África",
     subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    countries: 54,
+    languages: 23,
+    cities: 27,
+    imgUrl:
+      "https://images.unsplash.com/photo-1484318571209-661cf29a69c3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
   },
   {
     slug: "europe",
@@ -35,13 +57,23 @@ const continents = [
     subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    countries: 60,
+    languages: 23,
+    cities: 27,
+    imgUrl:
+      "https://images.unsplash.com/photo-1471874708433-acd480424946?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
   },
   {
     slug: "oceania",
-    title: "América do Sul",
+    title: "Oceania",
     subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    countries: 14,
+    languages: 23,
+    cities: 27,
+    imgUrl:
+      "https://images.unsplash.com/photo-1589330273594-fade1ee91647?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
   },
 ];
 
@@ -62,16 +94,14 @@ export function makeServer({ environment = "test" }) {
     routes() {
       this.namespace = "api";
 
-      this.get("/continents", () => {
-        return this.schema.all("continent");
+      this.get("/continents", (schema) => {
+        return schema.all("continent");
       });
 
       this.get("/continents/:slug", (schema, request) => {
         const { slug } = request.params;
 
-        return schema.findBy({
-          slug,
-        });
+        return schema.find("slug", slug);
       });
     },
   });
